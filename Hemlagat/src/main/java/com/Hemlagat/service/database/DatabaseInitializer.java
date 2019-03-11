a80156d218e2780572e9000a89a2a50b64ca5608
nurhusein <nurabd@student.chalmers.se>
2019-03-12 00:25:45 +0100

    data-int

--- Hemlagat/src/main/java/com/Hemlagat/service/database/DatabaseInitializer.java
+++ Hemlagat/src/main/java/com/Hemlagat/service/database/DatabaseInitializer.java
@@ -21,6 +21,32 @@ public class DatabaseInitializer {
         //om databas tom, fyll med data
         System.out.println("deployad!!!!!!");
         
+        Addb db = new Addb();
+        db.setTitle("PASTA CARBONARA");
+        db.setDescription("Spaghetti carbonara – en älskad favorit med rökt fläsk eller bacon och grädde! Lika bra till släktmiddagen som till fredagsmyset.");
+        db.setStatus("sold");
+        db.setAddress("Gothenburg");
+        db.setOther("hörsalsvägen 11");
+        db.setOther2("23344");
+        
+        db.setWeight(0.3);
+        db.setPrice(30.0);
+        db.setQuantity(5);
+        
+        db.setGluten(true);
+        db.setLactose(true);
+        db.setNuts(true);
+        db.setVegetarian(true);
+        
+        Userdb us = new Userdb();
+        us.setEmail("alla");
+        us.setPassword("11");
+        us.setUsername("11");
+        
+        db.setUserid(us);
+        userbFacade.create(us);
+       
+        addbFacade.create(db);
         
       
         
